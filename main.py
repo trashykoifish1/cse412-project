b6a6c7387a2c576d2e9b8be4f1cd9b56e87d8b65
Khoi Tran <112349723+trashykoifish1@users.noreply.github.com>
2023-11-30 00:46:23 -0700

    Finished ADD, EDIT, DELETE functions
    Implemented the ADD, EDIT, DELETE functions for students and professors. Incorporated the database into the code.

--- main.py
+++ main.py
@@ -1,50 +1,197 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+import psycopg2
+
+DATABASE_NAME = "cse412-project"
+DATABASE_HOST = "localhost"
+USER = "khoi"
+PASSWORD = "1234"
+PORT = "5432"
+students = []
 
 app = Flask(__name__)
 
-# Sample data (replace this with data fetched from the database)
-students = [
-    {"uni_id": 1, "first_name": "John", "last_name": "Doe", "phone_number": "1234567890",
-     "email": "john.doe@example.com", "addr": "123 Main St", "gpa": 3.8, "tuition": 5000.00, "enr_yr": "2022"},
-    {"uni_id": 2, "first_name": "Jane", "last_name": "Smith", "phone_number": "9876543210",
-     "email": "jane.smith@example.com", "addr": "456 Oak St", "gpa": 3.5, "tuition": 4800.00, "enr_yr": "2021"},
-]
+conn = psycopg2.connect(database=DATABASE_NAME,
+                        host=DATABASE_HOST,
+                        user=USER,
+                        password=PASSWORD,
+                        port=PORT)
+cursor = conn.cursor()
 
 
-@app.route('/')
+### ID Generation
+def gen_uni_id():
+    cursor.execute("""
+        SELECT MAX(uni_id)
+        FROM university_person
+    """)
+    output = cursor.fetchone()
+    print(output)
+    # print(output[0] + 1)
+    if output[0] is None:
+        return 100
+    else:
+        return output[0] + 1
+
+
+def gen_stu_id():
+    cursor.execute("""
+        SELECT MAX(stu_id)
+        FROM student
+    """)
+    output = cursor.fetchone()
+    print(output)
+    # print(output[0] + 1)
+    if output[0] is None:
+        return 1000
+    else:
+        return output[0] + 1
+
+
+def gen_fa_id():
+    cursor.execute("""
+        SELECT MAX(fa_id)
+        FROM faculty
+    """)
+    output = cursor.fetchone()
+    print(output)
+    # print(output[0] + 1)
+    if output[0] is None:
+        return 2000
+    else:
+        return output[0] + 1
+
+
+def gen_prof_id():
+    cursor.execute("""
+        SELECT MAX(prof_id)
+        FROM professor
+    """)
+    output = cursor.fetchone()
+    print(output)
+    # print(output[0] + 1)
+    if output[0] is None:
+        return 3000
+    else:
+        return output[0] + 1
+
+
+## Student back-end
+def fetch_student_with_id(stu_id):
+    query = """
+    SELECT first_name, last_name, phone_number, email, addr, gpa, tuition, enr_yr, gender
+    FROM university_person as up, student as s
+    WHERE up.uni_id = s.uni_id
+    AND s.stu_id = %s
+    """
+    cursor.execute(query, (stu_id,))
+    fetched = cursor.fetchone()
+    first_name = fetched[0]
+    last_name = fetched[1]
+    phone_number = fetched[2]
+    email = fetched[3]
+    addr = fetched[4]
+    gpa = fetched[5]
+    tuition = fetched[6]
+    enr_yr = fetched[7]
+    gender = fetched[8]
+    student = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "phone_number": phone_number,
+        "email": email,
+        "addr": addr,
+        "gpa": gpa,
+        "tuition": tuition,
+        "enr_yr": enr_yr,
+        "gender": gender,
+    }
+    return student
+
+
+def fetch_students():
+    output = []
+    cursor.execute("""SELECT stu_id, first_name, last_name, phone_number, email, addr, gpa, tuition, enr_yr
+    FROM university_person as up, student as s
+    WHERE up.uni_id = s.uni_id
+    ORDER BY stu_id;""")
+    fetched = cursor.fetchall()
+    for student in fetched:
+        stu_id = student[0]
+        first_name = student[1]
+        last_name = student[2]
+        phone_number = student[3]
+        email = student[4]
+        addr = student[5]
+        gpa = student[6]
+        tuition = student[7]
+        enr_yr = student[8]
+        output.append({
+            "stu_id": stu_id,
+            "first_name": first_name,
+            "last_name": last_name,
+            "phone_number": phone_number,
+            "email": email,
+            "addr": addr,
+            "gpa": gpa,
+            "tuition": tuition,
+            "enr_yr": enr_yr,
+        })
+    return output
+
+
+@app.route('/manage_students')
 def index():
-    return render_template('index.html', students=students)
+    return render_template('index.html', students=fetch_students())
 
 
-@app.route('/edit/<int:uni_id>', methods=['GET', 'POST'])
-def edit_student(uni_id):
-    student = next((s for s in students if s['uni_id'] == uni_id), None)
-
+@app.route('/manage_students/edit/<int:stu_id>', methods=['GET', 'POST'])
+def edit_student(stu_id):
     if request.method == 'POST':
-        # Update the student information (replace this with database update)
-        student['first_name'] = request.form['first_name']
-        student['last_name'] = request.form['last_name']
-        student['phone_number'] = request.form['phone_number']
-        student['email'] = request.form['email']
-        student['addr'] = request.form['addr']
-        student['gpa'] = float(request.form['gpa'])
-        student['tuition'] = float(request.form['tuition'])
-        student['enr_yr'] = request.form['enr_yr']
-
+        # Update the student information
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        phone_number = request.form['phone_number']
+        email = request.form['email']
+        addr = request.form['addr']
+        gpa = float(request.form['gpa'])
+        tuition = float(request.form['tuition'])
+        enr_yr = request.form['enr_yr']
+        gender = request.form['gender']
+        query = """
+            UPDATE university_person as up
+            SET first_name = %s, last_name = %s, phone_number = %s, email = %s, addr = %s, gender = %s
+            FROM student as s
+            WHERE s.uni_id = up.uni_id
+            AND s.stu_id = %s;
+        """
+        cursor.execute(query, (first_name, last_name, phone_number, email, addr, gender, stu_id))
+        query = """
+            UPDATE student
+            SET gpa = %s, tuition = %s, enr_yr = %s
+            WHERE stu_id = %s
+        """
+        cursor.execute(query, (gpa, tuition, enr_yr, stu_id))
+        conn.commit()
         return redirect(url_for('index'))
 
-    return render_template('edit_student.html', student=student)
+    return render_template('edit_student.html', student=fetch_student_with_id(stu_id))
 
 
-@app.route('/delete/<int:uni_id>')
-def delete_student(uni_id):
-    # Delete the student (replace this with database delete)
-    global students
-    students = [s for s in students if s['uni_id'] != uni_id]
+@app.route('/manage_students/delete/<int:stu_id>')
+def delete_student(stu_id):
+    # Delete the student
+    query = """
+        DELETE FROM university_person
+        USING student
+        WHERE student.uni_id = university_person.uni_id
+        AND student.stu_id = %s;
+    """
+    cursor.execute(query, (stu_id,))
+    conn.commit()
     return redirect(url_for('index'))
 
 
-@app.route('/add_student', methods=['GET', 'POST'])
+@app.route('/manage_students/add_student', methods=['GET', 'POST'])
 def add_student():
     if request.method == 'POST':
         # Fetch form data
@@ -56,27 +203,168 @@ def add_student():
         add_gpa = request.form['add_gpa']
         add_tuition = request.form['add_tuition']
         add_enrollment_year = request.form['add_enrollment_year']
+        add_gender = request.form['add_gender']
 
-        # Generate a unique uni_id (replace this with uni_id logic)
-        new_uni_id = len(students) + 1
-
-        # Add the new student to the list (replace this with database insert)
-        students.append({
-            'uni_id': new_uni_id,
-            'first_name': add_first_name,
-            'last_name': add_last_name,
-            'phone_number': add_phone_number,
-            'email': add_email,
-            'addr': add_address,
-            'gpa': add_gpa,
-            'tuition': add_tuition,
-            'enr_yr': add_enrollment_year,
-            # Add other attributes for the new student
-        })
-
+        # Generate a unique IDs
+        new_stu_id = gen_stu_id()
+        new_uni_id = gen_uni_id()
+        query = """
+            INSERT INTO university_person VALUES
+            (%s, %s, %s, %s, %s, %s, %s);
+            INSERT INTO student VALUES
+            (%s, %s, %s, %s, %s);
+        """
+        cursor.execute(query, (new_uni_id, add_first_name, add_last_name, add_phone_number, add_email, add_address,
+                               add_gender, new_uni_id, new_stu_id, float(add_gpa), float(add_tuition),
+                               add_enrollment_year))
+        conn.commit()
         return redirect(url_for('index'))
 
     return render_template('add_student.html')
+
+
+### Manage professor back-end:
+
+def fetch_profs():
+    output = []
+    cursor.execute("""
+        SELECT prof_id, first_name, last_name, phone_number, email, addr, salary
+        FROM university_person as up, professor as p, faculty as f
+        WHERE up.uni_id = p.uni_id
+        AND p.uni_id = f.uni_id
+        ORDER BY prof_id;
+    """)
+    fetched = cursor.fetchall()
+    for prof in fetched:
+        prof_id = prof[0]
+        first_name = prof[1]
+        last_name = prof[2]
+        phone_number = prof[3]
+        email = prof[4]
+        addr = prof[5]
+        salary = prof[6]
+        output.append({
+            "prof_id": prof_id,
+            "first_name": first_name,
+            "last_name": last_name,
+            "phone_number": phone_number,
+            "email": email,
+            "addr": addr,
+            "salary": salary,
+        })
+    return output
+
+
+def fetch_professor_with_id(prof_id):
+    query = """
+        SELECT first_name, last_name, phone_number, email, addr, salary, gender
+        FROM university_person as up, professor as p, faculty as f
+        WHERE up.uni_id = p.uni_id
+        AND p.uni_id = f.uni_id
+        AND p.prof_id = %s
+    """
+    cursor.execute(query, (prof_id,))
+    fetched = cursor.fetchone()
+    first_name, last_name, phone_number, email, addr, salary, gender = fetched
+    professor = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "phone_number": phone_number,
+        "email": email,
+        "addr": addr,
+        "salary": salary,
+        "gender": gender,
+    }
+    return professor
+
+
+@app.route('/manage_professors')
+def manage_prof():
+    return render_template('manage_prof.html', professors=fetch_profs())
+
+
+@app.route('/manage_professors/add_professor', methods=['GET', 'POST'])
+def add_prof():
+    if request.method == 'POST':
+        # Fetch form data
+        add_first_name = request.form['add_first_name']
+        add_last_name = request.form['add_last_name']
+        add_phone_number = request.form['add_phone_number']
+        add_email = request.form['add_email']
+        add_address = request.form['add_address']
+        add_salary = request.form['add_salary']
+        add_gender = request.form['add_gender']
+
+        # Generate unique IDs
+        new_prof_id = gen_prof_id()
+        new_fa_id = gen_fa_id()
+        new_uni_id = gen_uni_id()
+        query = """
+                INSERT INTO university_person VALUES
+                (%s, %s, %s, %s, %s, %s, %s);
+                INSERT INTO faculty VALUES
+                (%s, %s, %s);
+                INSERT INTO professor VALUES
+                (%s, %s, %s);
+            """
+        cursor.execute(query, (new_uni_id, add_first_name, add_last_name, add_phone_number, add_email, add_address,
+                               add_gender, new_uni_id, new_fa_id, float(add_salary),
+                               new_uni_id, new_fa_id, new_prof_id))
+        conn.commit()
+        return redirect(url_for('manage_prof'))
+
+    return render_template('add_prof.html')
+
+
+@app.route('/manage_professors/delete/<int:prof_id>')
+def delete_prof(prof_id):
+    # Delete the professor
+    query = """
+        DELETE FROM university_person
+        USING professor
+        WHERE professor.uni_id = university_person.uni_id
+        AND professor.prof_id = %s;
+    """
+    cursor.execute(query, (prof_id,))
+    conn.commit()
+    return redirect(url_for('manage_prof'))
+
+
+@app.route('/manage_professors/edit/<int:prof_id>', methods=['GET', 'POST'])
+def edit_prof(prof_id):
+    if request.method == 'POST':
+        # Update the professor information
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        phone_number = request.form['phone_number']
+        email = request.form['email']
+        addr = request.form['addr']
+        salary = float(request.form['salary'])
+        gender = request.form['gender']
+
+        query = """
+            UPDATE university_person as up
+            SET first_name = %s, last_name = %s, phone_number = %s, email = %s, addr = %s, gender = %s
+            FROM professor as p, faculty as f
+            WHERE p.uni_id = up.uni_id
+            AND p.uni_id = f.uni_id
+            AND p.prof_id = %s;
+        """
+        cursor.execute(query, (first_name, last_name, phone_number, email, addr, gender, prof_id))
+
+        query = """
+            UPDATE faculty
+            SET salary = %s
+            WHERE uni_id IN (
+                SELECT uni_id FROM professor WHERE prof_id = %s
+            );
+        """
+        cursor.execute(query, (salary, prof_id))
+
+        conn.commit()
+        return redirect(url_for('manage_prof'))
+
+    return render_template('edit_prof.html', professor=fetch_professor_with_id(prof_id))
 
 
 if __name__ == '__main__':
